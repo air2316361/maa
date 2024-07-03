b7401744ed5d2f410ab4188187c2f6135ce6ab83
中国特色社会主义码农 <air2316361@sina.com>
2024-07-03 17:22:50 +0800

    111

---
+++ src/main/java/com/maa/remote/service/WebsiteSchedule.java
@@ -0,0 +1,21 @@
+package com.maa.remote.service;
+
+import jakarta.annotation.Resource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+@Component
+public class WebsiteSchedule {
+    @Resource
+    private RestTemplate restTemplate;
+    @Value("${DOMAIN:gai.cloudns.org}")
+    private String domain;
+
+    @Scheduled(cron = "0 * * * * *")
+    public void schedule() {
+        restTemplate.getForObject("https://" + domain, String.class);
+    }
+}
